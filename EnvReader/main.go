b6f3c248955eaa01805fd1cf7306d25b6c7429d4
DevClusterRu <82021801+DevClusterRu@users.noreply.github.com>
2021-04-08 09:05:42 +0700

    Create main.go

---
+++ EnvReader/main.go
@@ -0,0 +1,49 @@
+package EnvReader
+
+import (
+	"github.com/kardianos/osext"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+var Env map[string]string
+
+func ConfigInit() map[string]string {
+
+	Device = make(map[int]DeviceMap)
+
+	Env = make(map[string]string)
+	folderPath, err := osext.ExecutableFolder()
+
+	if err != nil {
+		log.Println("Cant find folder path", err)
+	}
+
+	file, err := os.Open(folderPath + `/config.conf`)
+
+	if err != nil {
+		file, err = os.Open("config.conf")
+	}
+
+	if err != nil {
+		log.Println("Cant open config file", err)
+		os.Exit(0)
+	}
+
+	b, _ := ioutil.ReadAll(file)
+	rows := strings.Split(string(b), "\n")
+	for _, v := range rows {
+		if v == "" || strings.Index(v, "#") == 0 {
+			continue
+		}
+
+		pair := strings.Split(v, "=")
+		if len(pair) == 2 {
+			pair[0] = strings.TrimSpace(pair[0])
+			pair[1] = strings.TrimSpace(pair[1])
+			Env[pair[0]] = pair[1]
+		}
+	}
+}
